2ad2432c2b8e2f8b057a49a89756a1812db64254
Tamas Beszedics <beszedicshun@gmail.com>
2022-05-09 20:05:06 +0200

    Add story functionality (query on dashboard, add new one in form)

--- app.js
+++ app.js
@@ -21,13 +21,22 @@ connectDB();
 
 const app = express();
 
+// Body parser
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
 // Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Handlebars helpers
+const { formatDate } = require('./helpers/hbs');
+
 // Handlebars
-app.engine('.hbs', exphbs.engine({extname: '.hbs', defaultLayout: 'main'}));
+app.engine('.hbs', exphbs.engine({helpers: {
+  formatDate,
+}, extname: '.hbs', defaultLayout: 'main'}));
 app.set('view engine', '.hbs');
 
 // Sessions
@@ -53,6 +62,7 @@ app.use('*/images',express.static('public/images'));
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
 
 const PORT = process.env.PORT || 8080
 
